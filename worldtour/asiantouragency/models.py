f9ff7b81f0f4dcd62ccc2938284da32516af019a
otodavid <ojooloruntomi@gmail.com>
2024-08-20 01:39:55 -0500

    database setup

--- worldtour/asiantouragency/models.py
+++ worldtour/asiantouragency/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    origin_country = models.CharField(max_length=64)
+    desitination_country = models.CharField(max_length=64)
+    number_of_nights = models.IntegerField()
+    price = models.IntegerField()
--- worldtour/asiantouragency/urls.py
+++ worldtour/asiantouragency/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
  
 #create a list of url patterns
-url_patterns = [
+urlpatterns = [
     path('', views.index)
 ] 
