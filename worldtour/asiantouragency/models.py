3cd039db0bd193fd4d48bc0f325609d9e50a167c
otodavid <ojooloruntomi@gmail.com>
2024-08-20 22:22:34 -0500

    added records to database

--- worldtour/asiantouragency/models.py
+++ worldtour/asiantouragency/models.py
@@ -3,6 +3,11 @@ from django.db import models
 # Create your models here.
 class Tour(models.Model):
     origin_country = models.CharField(max_length=64)
-    desitination_country = models.CharField(max_length=64)
+    dest_country = models.CharField(max_length=64)
     number_of_nights = models.IntegerField()
     price = models.IntegerField()
+    
+    # This is a string representation of the Tour class
+    def __str__(self):
+        return f"ID {self.id}: From {self.origin_country} to {self.dest_country}. Price is {self.price} for {self.number_of_nights} nights"
+        
